e9ef0f0e50ad2590b2c445f580d19aafa9b2a9b2
Edison Osorio <edisonosorio@nusoft.com.co>
2017-02-07 17:52:11 -0500

    Avances iniciales

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-index.php
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Softaller</title>
+</head>
+<body>
+ <h1>Centro Electrónico</h1>
+ <div>
+ 	<form>
+ 		<label>Buscar:</label>
+ 		<input type="text" name="buscar">
+ 		<input type="input" name="buscar">
+ 	</form>
+ </div>
+</body>
+</html>
