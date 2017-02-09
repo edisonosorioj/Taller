96ac8e65d32fbd50a888039e17dc36be384f2b60
edisonosorioj <edisonosorioj@gmail.com>
2017-02-08 20:25:14 -0500

    Conexión a base de datos

--- index.php
+++ index.php
@@ -1,26 +1,54 @@
-<!DOCTYPE html>
+<?php  
+require_once 'conexion.php';
+
+$conex = new conection();
+$result = $conex->conex();
+
+$tr='';
+$query = mysqli_query($result,'select * from repuestos');
+
+ while ($row = $query->fetch_array(MYSQLI_BOTH)){
+
+ 	$tr .=	"<tr class='rows' id='rows'>
+				<td>" . $row['id'] 			. "</td>
+				<td>" . $row['caja'] 		. "</td>
+				<td>" . $row['articulo'] 	. "</td>
+				<td>" . $row['ecg'] 		. "</td>
+				<td>" . $row['precio'] 		. "</td>
+			</tr>";
+
+ }
+
+
+$html = "<!DOCTYPE html>
 <html>
 <head>
+	<meta charset='UTF-8' />
 	<title>Softaller</title>
+	<link rel='stylesheet' href='css/estilo.css' />
 </head>
 <body>
  <h1>Centro Electrónico</h1>
  <div>
  	<form>
  		<label>Buscar:</label>
- 		<input type="text" name="buscar" placeholder="Texto">
- 		<input type="submit" name="buscar">
+ 		<input type='text' name='buscar' placeholder='Texto'>
+ 		<input type='submit' name='buscar'>
  	</form>
  </div>
- <div>
+ <div id='table'>
  	<table>
- 		<td>
- 			<tr></tr>
- 			<tr></tr>
- 			<tr></tr>
- 			<tr></tr>
- 		</td>
+ 		<tr>
+ 			<td>ID</td>
+ 			<td>Caja</td>
+ 			<td>Articulo</td>
+ 			<td>ECG</td>
+ 			<td>Precio</td>
+ 		</tr>
+		. $tr . 
  	</table>
  </div>
 </body>
-</html>
+</html>";
+
+echo $html;
